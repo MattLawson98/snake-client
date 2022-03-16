8d84ac099ddc6a33f23c19b8bcb8ff45f3342371
MattLawson98 <matthewlawson50@gmail.com>
2022-03-16 14:06:44 -0700

    This file has some controls for the snake game

--- play.js
+++ play.js
@@ -1,8 +1,17 @@
 const net = require("net");
 const {conn,connect} = require('./client')
 
-// establishes a connection with the game server
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
-
-console.log("Connecting ...");
-connect();
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
